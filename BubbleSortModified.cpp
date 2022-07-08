294f1dfc8e9941d5d61a5e4e3d702279cff2079c
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-07-08 23:27:24 +0600

    Modified bubble sort algorithm

---
+++ BubbleSortModified.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file BubbleSortModified.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief Modified Bubble Sort Algorithm Implementation
+ * @version 0.1
+ * @date 2022-07-08
+ * @since Friday; 10:53AM
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+void bubbleSortModified(int A[], unsigned int n){
+    int count = 0;
+    bool flag = true;
+    for(unsigned int i = 0; i < n-1 && flag == true; i++){
+        flag = false;
+        for(unsigned int j = 0; j < n-(i+1); j++){
+            if(A[j] > A[j+1]){
+                swap(A[j], A[j+1]);
+                flag = true;
+            }
+        }
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int A[] = {1, 2, 3, 4, 5};
+    unsigned int n = sizeof(A) / sizeof(int);
+    bubbleSortModified(A, n);
+    for(auto x : A){
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+    
+}
